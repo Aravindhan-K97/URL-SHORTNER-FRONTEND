271c0e33853bd73487f0ebc12a5669b8665a4f15
Aravindhan-K97 <aravindhan2825@gmail.com>
2024-09-25 02:17:43 +0530

    Final commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
           <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/activate/:token" element={<ActivateAccount />} />
+          <Route path="/activate/:activationToken" element={<ActivateAccount />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/reset-password/:resetToken" element={<ResetPassword />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/url-stats" element={<UrlStats />} />
           <Route path="/url-list" element={<UrlList />} />
